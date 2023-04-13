b9a2ef7d79466743660ee4257a1bff4d76046c36
shiv-iyer <shiviyer55x@gmail.com>
2023-04-13 09:28:22 +0000

    added alan and his cat

--- notes.sql
+++ notes.sql
@@ -1,3 +1,5 @@
 INSERT INTO posters (title, cost, decsription, date, stock, height, width) values ("The Story of Mario", 125, "This tells the story of an Italian plumber, Mario", "04-13-2023", 4, 100, 50);
 
 INSERT INTO posters (title, cost, decsription, date, stock, height, width) values ("Haikal and Eren", 700, "The story of a dynamic father and son duo who conquer the world. While stocks last", "04-13-2023", 1, 150, 150);
+
+INSERT INTO posters (title, cost, decsription, date, stock, height, width) values ("Alan and The Forbidden Cat", 100, "The epic tale of a Malaysian-Chinese legend who adopted a super cute cat and then sold it on his Project 2 website...", "04-13-2023", 1, 300, 300);
