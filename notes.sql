8ee5a74a2c50b04fb8c8876baada46961c6f885b
shiv-iyer <shiviyer55x@gmail.com>
2023-04-13 09:18:53 +0000

    random styling and added haikal and eren

--- notes.sql
+++ notes.sql
@@ -1,1 +1,3 @@
 INSERT INTO posters (title, cost, decsription, date, stock, height, width) values ("The Story of Mario", 125, "This tells the story of an Italian plumber, Mario", "04-13-2023", 4, 100, 50);
+
+INSERT INTO posters (title, cost, decsription, date, stock, height, width) values ("Haikal and Eren", 700, "The story of a dynamic father and son duo who conquer the world. While stocks last", "04-13-2023", 1, 150, 150);
