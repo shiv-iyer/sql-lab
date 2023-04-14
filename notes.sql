e9172e2327804450dda2045fbda1aca3117e4fe9
shiv-iyer <shiviyer55x@gmail.com>
2023-04-14 05:12:02 +0000

    notes for css file

--- notes.sql
+++ notes.sql
@@ -3,3 +3,5 @@ INSERT INTO posters (title, cost, decsription, date, stock, height, width) value
 INSERT INTO posters (title, cost, decsription, date, stock, height, width) values ("Haikal and Eren", 700, "The story of a dynamic father and son duo who conquer the world. While stocks last", "04-13-2023", 1, 150, 150);
 
 INSERT INTO posters (title, cost, decsription, date, stock, height, width) values ("Alan and The Forbidden Cat", 100, "The epic tale of a Malaysian-Chinese legend who adopted a super cute cat and then sold it on his Project 2 website...", "04-13-2023", 1, 300, 300);
+
+For a css file in hbs, can link it in base.hbs and anything that extends base can use it.
